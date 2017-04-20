a0544e4d6f13c019f3cacd89def1d67e97ab3b58
aftabsikander <aftab.a.sikander@gmail.com>
2017-04-20 19:29:07 +0500

    completed classification for SVM
    Signed-off-by: aftabsikander <aftab.a.sikander@gmail.com>

--- mint_dataset_train.py
+++ mint_dataset_train.py
@@ -33,13 +33,14 @@ def read(dataset="training", path="."):
         yield get_img(i)
 
 
-def show(image):
+def show(image, prediction):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     imgplot = plt.imshow(image, cmap=plt.cm.gray)
     imgplot.set_interpolation('nearest')
-    ax.xaxis.set_ticks_position('top')
+    ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
+    plt.title('Prediction: %i' % prediction)
     plt.show()
 
 
@@ -96,39 +97,15 @@ n_labels = len(training_labels)
 dataLabels = training_labels.reshape((n_labels, -1))
 # show(images[1])
 
-x_data = dataImage[:5000]
-y_data = dataLabels[:5000]
+x_data = dataImage[:300]
+y_data = dataLabels[:300]
 print(x_data[1].shape)
 print(y_data[1].shape)
 # Applying classification
 classifier = svm.SVC(gamma=0.0001, C=100)
-classifier.fit(x_data, y_data)
+classifier.fit(x_data, y_data.ravel())
 
-predict = classifier.predict(training_images[6000])
+print(dataImage[110])
+predict = classifier.predict(dataImage[122])
 print('result label:', predict)
-show(training_images[6000])
-
-""" 
-#Working code
-training_data = list(read(dataset='training',
-                          path='/media/afali/Project/Projects/Projects/Python-workspace/hello-scikitlearn/Dataset/'))
-training_label, training_pixels = training_data[3]
-print(training_label)
-print(training_pixels.shape)
-"""
-
-"""
-testing_data = list(
-    read(dataset='testing',
-         path='/media/afali/Project/Projects/Projects/Python-workspace/hello-scikitlearn/Dataset/'))
-testing_label, testing_pixels = testing_data[1]
-print(testing_label)
-print(testing_pixels.shape)
-
-clf = svm.SVC(gamma=0.0001, C=100)
-clf.fit(training_label, training_pixels)
-predict = clf.predict(testing_pixels)
-print('result label:', predict)
-"""
-
-# show(testing_pixels)
+show(training_images[122], predict)
